f234f9dba94ae99c2e62f55dc238e1063017cda5
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-01-26 01:18:56 +0100

    Added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Company;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +25,20 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $adminRole = Role::create(['guard_name' => 'web', 'name' => 'admin']);
+        $shareholderRole = Role::create(['guard_name' => 'web', 'name' => 'shareholder']);
+
+        $admin = User::create([
+            'name' => 'Admin Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $admin->assignRole($adminRole);
+
+        Company::create([
+            'name' => 'Dangote Cement',
+            'acronym' => 'DANGCEM'
+        ]);
     }
 }
