2625cdf8df185fa0c8b342d8ca10e85925d4148c
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-01-26 01:37:33 +0100

    Structured endpoint.

--- routes/api.php
+++ routes/api.php
@@ -24,10 +24,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login', [LoginController::class, 'login']);
 
+// Admin routes
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::post('/shareholders', [ShareholderController::class, 'store']);
+    Route::post('/voting-items', [VotingItemController::class, 'store']);
+    Route::get('/votes', [VoteController::class, 'index']);
+});
+
 Route::middleware('auth:sanctum')->get('/voting-items', [VotingItemController::class, 'index']);
-Route::middleware('auth:sanctum')->post('/voting-items', [VotingItemController::class, 'store']);
-
-Route::middleware('auth:sanctum')->post('/shareholders', [ShareholderController::class, 'store']);
-
 Route::middleware('auth:sanctum')->post('/votes', [VoteController::class, 'store']);
-Route::middleware('auth:sanctum')->get('/votes', [VoteController::class, 'index']);
